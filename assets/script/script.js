4c3c67f8b33b16483ac2c9f4cb86cb906a540961
jahid-sourav <info.jahidsourav@gmail.com>
2021-04-21 10:34:59 +0600

    Completed Toggle Menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,8 @@
-(function ($) {
-	"use strict";
-})(jQuery);
-
+// Toggle Menu Starts Here
+function openNav() {
+	document.getElementById("mainNav").style.height = "100%";
+}
+function closeNav() {
+	document.getElementById("mainNav").style.height = "0%";
+}
+// Toggle Menu Ends Here
